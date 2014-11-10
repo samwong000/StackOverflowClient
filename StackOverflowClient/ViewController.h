818301e9d842c155ddbf9a2146ef96b748b83576
Sam Wong <sam.wong.uk@gmail.com>
2014-11-10 13:47:42 -0800

    Initial Commit

---
+++ StackOverflowClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StackOverflowClient
+//
+//  Created by Sam Wong on 10/11/2014.
+//  Copyright (c) 2014 Sam Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StackOverflowClientTests/StackOverflowClientTests.m
@@ -0,0 +1,40 @@
+//
+//  StackOverflowClientTests.m
+//  StackOverflowClientTests
+//
+//  Created by Sam Wong on 10/11/2014.
+//  Copyright (c) 2014 Sam Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface StackOverflowClientTests : XCTestCase
+
+@end
+
+@implementation StackOverflowClientTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
